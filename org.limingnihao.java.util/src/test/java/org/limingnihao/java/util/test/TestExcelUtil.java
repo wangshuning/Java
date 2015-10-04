d6eeddcbafa99bebe8219bae40f47d583052b3b7
limingnihao <limingnihao@live.com>
2015-10-04 11:47:05 +0800

    123

---
+++ org.limingnihao.java.util/src/test/java/org/limingnihao/java/util/test/TestExcelUtil.java
@@ -0,0 +1,13 @@
+package org.limingnihao.java.util.test;
+
+import org.junit.Test;
+import org.limingnihao.util.ExcelUtil;
+
+public class TestExcelUtil {
+
+	@Test
+	public void main() {
+		ExcelUtil.importExcel("/Users/lishiming/Downloads/boss.xls", 0);
+	}
+
+}
