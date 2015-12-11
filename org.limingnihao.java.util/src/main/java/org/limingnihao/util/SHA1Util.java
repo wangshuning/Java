49f0911a80b0f469e2e15312a177a3de0eba180a
jangwind <jang_wind@sina.com>
2015-12-11 12:13:52 +0800

    @add 新增获取文件SHA1方法
    Signed-off-by: jangwind <jang_wind@sina.com>

--- org.limingnihao.java.util/src/main/java/org/limingnihao/util/SHA1Util.java
+++ org.limingnihao.java.util/src/main/java/org/limingnihao/util/SHA1Util.java
@@ -1,35 +1,93 @@
 package org.limingnihao.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA1Util {
 
-    public static String SHA1(String decript) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            digest.update(decript.getBytes());
-            byte messageDigest[] = digest.digest();
-            // Create Hex String
-            StringBuffer hexString = new StringBuffer();
-            // 字节数组转换为 十六进制 数
-            for (int i = 0; i < messageDigest.length; i++) {
-                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
-                if (shaHex.length() < 2) {
-                    hexString.append(0);
-                }
-                hexString.append(shaHex);
-            }
-            return hexString.toString();
+	public static String SHA1(String decript) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+			digest.update(decript.getBytes());
+			byte messageDigest[] = digest.digest();
+			// Create Hex String
+			StringBuffer hexString = new StringBuffer();
+			// 字节数组转换为 十六进制 数
+			for (int i = 0; i < messageDigest.length; i++) {
+				String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+				if (shaHex.length() < 2) {
+					hexString.append(0);
+				}
+				hexString.append(shaHex);
+			}
+			return hexString.toString();
 
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
 
-    public static void main(String[] args) {
-        String result = SHA1Util.SHA1("123123");
-        System.out.println(result);
-    }
+	/**
+	 * 获取文件SHA1
+	 * @param file 文件对象【java.io.File】
+	 * @return
+	 */
+	public static String SHA1(File file) {
+
+		FileInputStream in = null;
+		
+		try {
+			in = new FileInputStream(file);
+			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+			byte[] buffer = new byte[1024 * 1024 * 10];
+			int len = 0;
+
+			while ((len = in.read(buffer)) > 0) {
+				// 该对象通过使用 update（）方法处理数据
+				digest.update(buffer, 0, len);
+			}
+			
+			byte messageDigest[] = digest.digest();
+			// Create Hex String
+			StringBuffer hexString = new StringBuffer();
+			// 字节数组转换为 十六进制 数
+			for (int i = 0; i < messageDigest.length; i++) {
+				String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+				if (shaHex.length() < 2) {
+					hexString.append(0);
+				}
+				hexString.append(shaHex);
+			}
+			return hexString.toString();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != in) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}  
+			}
+		}
+
+		return "";
+	}
+
+	public static void main(String[] args) {
+		/*String result = SHA1Util.SHA1("123123");
+		System.out.println(result);*/
+		File file = new File("d:/project/产品/观影、相册、设备绑定/v1.1/接口设计.docx");
+		String result = SHA1Util.SHA1(file);
+		System.out.println(result);
+	}
 }
