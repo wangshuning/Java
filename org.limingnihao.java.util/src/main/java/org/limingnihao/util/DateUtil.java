ae7ba076f6227b2b94bda9fd682648aa4f1ae469
codelipenghui <309994536@qq.com>
2016-03-25 15:01:56 +0800

    更新dateutil

--- org.limingnihao.java.util/src/main/java/org/limingnihao/util/DateUtil.java
+++ org.limingnihao.java.util/src/main/java/org/limingnihao/util/DateUtil.java
@@ -724,4 +724,21 @@ public class DateUtil {
         return bean;
     }
 
+    /**
+     * 获取日期的开始时间
+     * @param date
+     * @return
+     */
+    public static Date getDateBegin(Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
+
+
 }
